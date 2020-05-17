af79091b14a72c9620af90af1a16cfbc66d7fbed
Debaprasad-dez <59891877+Debaprasad-dez@users.noreply.github.com>
2020-05-17 15:18:04 +0530

    Add files via upload

---
+++ don.js
@@ -0,0 +1,53 @@
+const myFunction = () => {
+  document.getElementById("first").style.display = "block";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+  document.getElementById("sixth").style.display = "none";
+};
+
+const myFunction2 = () => {
+  document.getElementById("second").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("third").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+  document.getElementById("sixth").style.display = "none";
+};
+
+const myFunction3 = () => {
+  document.getElementById("third").style.display = "block";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+  document.getElementById("sixth").style.display = "none";
+};
+
+const myFunction4 = () => {
+  document.getElementById("third").style.display = "none";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("fourth").style.display = "block";
+  document.getElementById("fifth").style.display = "none";
+  document.getElementById("sixth").style.display = "none";
+};
+
+const myFunction5 = () => {
+  document.getElementById("third").style.display = "none";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+  document.getElementById("fifth").style.display = "block";
+  document.getElementById("sixth").style.display = "none";
+};
+
+const myFunction6 = () => {
+  document.getElementById("third").style.display = "none";
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("fourth").style.display = "none";
+  document.getElementById("fifth").style.display = "none";
+  document.getElementById("sixth").style.display = "block";
+};
